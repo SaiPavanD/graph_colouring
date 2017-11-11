654f9afce28b030b65e30f314284056b25a077e5
Sai Pavan D <saipvn4@gmail.com>
2017-11-11 23:07:11 +0530

    updated kernels.h

--- kernels.h
+++ kernels.h
@@ -1,16 +1,19 @@
 
 /* Include this file in your main.cu */
 
-#ifndef KERNEL_H 
+#ifndef KERNEL_H
 #define KERNEL_H
+
+__global__ void check_correctness (unsigned int num_nodes, unsigned int *offset_arr,
+                                 unsigned int *cols_arr,  int *color_assignment, bool *result);
+
+__host__ void mis_coloring(unsigned int num_nodes, unsigned int *offset_arr,
+                                unsigned int *cols_arr, int *color_assignment);
 
 __host__ void jpl_coloring(unsigned int num_nodes, unsigned int *offset_arr,
                                  unsigned int *cols_arr, int *color_assignment);
 
-__host__ void mis_coloring(unsigned int num_nodes, unsigned int *offset_arr,
-                                 unsigned int *cols_arr, int *color_assignment);
-
-__global__ void check_correctness (unsigned int num_nodes, unsigned int *offset_arr,
-                                 unsigned int *cols_arr,  int *color_assignment, bool *result);
+__host__ void ldf_coloring(unsigned int num_nodes, unsigned int *offset_arr,
+                                unsigned int *cols_arr, int *color_assignment);
 
 #endif
