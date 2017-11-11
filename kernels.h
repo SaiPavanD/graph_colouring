5f9d0d40f1ab38c5b590e4a83600498fbe7198cc
seshu <seshu.nine6@gmail.com>
2017-11-11 20:54:08 +0530

    adding main files

---
+++ kernels.h
@@ -0,0 +1,11 @@
+
+/* Include this file in your main.cu */
+
+#ifndef KERNEL_H 
+#define KERNEL_H
+
+__global__ void mis_coloring_kernel(unsigned int num_nodes, unsigned int color, unsigned int *offset_arr,
+
+__global__ void jpl_coloring_kernel(unsigned int num_nodes, unsigned int color, unsigned int *offset_arr,
+
+#endif
